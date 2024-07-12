f97476256cccdbafa09961ddbd2909198da2ffeb
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-07-13 00:14:37 +0300

    ErrorMessage component done

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,13 +1,17 @@
 import { FaRegFaceFrown } from 'react-icons/fa6';
 import css from './ErrorMessage.module.css'
+import React, { FC } from 'react';
 
-export default function ErrorMessage() {
+const ErrorMessage: FC = () => {
   return (
     <div>
       <p className={css.error}>
-        Something went wrong! Please, reload the page!<br/>
-        <FaRegFaceFrown className={ css.errorIcon} />
+        Something went wrong! Please, reload the page!
+        <br />
+        <FaRegFaceFrown className={css.errorIcon} />
       </p>
     </div>
   );
-}
+};
+
+export default ErrorMessage;
