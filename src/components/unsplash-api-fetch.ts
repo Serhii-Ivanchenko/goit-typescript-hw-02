6356b90042077a81b54a9e375dd5e5d92ea1d12c
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-07-12 18:24:57 +0300

    transfer the project to ts + typification of api function

--- src/components/unsplash-api-fetch.ts
+++ src/components/unsplash-api-fetch.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.unsplash.com/';
 const ACCESS_KEY = 'GnQjZdKq8ut7mPn4cKm-uRXIzb8FNJgqXoZUMLmbnm8';
 
-export default async function getPhotos(searchQuery, page) {
+export default async function getPhotos<T>(searchQuery: string, page: number): Promise<T> {
   const response = await axios.get(`/search/photos`, {
     params: {
       client_id: ACCESS_KEY,
@@ -12,5 +12,5 @@ export default async function getPhotos(searchQuery, page) {
       per_page: 12,
     },
   });
-  return response.data;
+  return response.data as T;
 }
