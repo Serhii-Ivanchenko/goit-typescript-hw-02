2e6a69bafa8afee4884458c768c69d9b98ea41a2
Serhii-Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-07-13 11:46:04 +0300

    add types to LoadMoreBtn component

--- src/components/LoadMoreBtn/LoadMoreBtn.tsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.tsx
@@ -1,9 +1,16 @@
-import css from './LoadMoreBtn.module.css'
+import { FC } from 'react';
+import { HandleLoadMoreClick } from '../App/App.types';
+import css from './LoadMoreBtn.module.css';
 
-export default function LoadMoreBtn({ onClick }) {
+interface LoadMoreBtnProps {
+  onClick: HandleLoadMoreClick;
+}
+
+const LoadMoreBtn: FC<LoadMoreBtnProps> = ({ onClick }) => {
   return (
     <button className={css.loadMoreBtn} type="button" onClick={onClick}>
       Load more
     </button>
   );
-}
+};
+export default LoadMoreBtn;
